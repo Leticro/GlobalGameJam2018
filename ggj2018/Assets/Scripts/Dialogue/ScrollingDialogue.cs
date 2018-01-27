5e12b1d68591eb0f9253da750bfe1fd89c69fb62
Shane <shanemcdermott@unomaha.edu>
2018-01-27 12:37:39 -0600

    Added Scrolling Dialogue

---
+++ ggj2018/Assets/Scripts/Dialogue/ScrollingDialogue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingDialogue : MonoBehaviour
+{
+    
+    public string dialogue;
+    public int charsPerSec = 8;
+    public Text text;
+
+    private int dialogueIndex = 0;
+    private float timer;
+    private float charRate;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+    private void Awake()
+    {
+        dialogueIndex = 0;
+        timer = 0f;
+        charRate = 1f/charsPerSec;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (dialogueIndex < dialogue.Length - 1)
+        {
+            timer += Time.deltaTime;
+            if (timer >= charRate)
+            {
+                text.text = dialogue.Substring(0, ++dialogueIndex);
+                timer -= charRate;
+            }
+        }		
+	}
+
+}
