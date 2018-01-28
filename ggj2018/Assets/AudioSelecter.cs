d79ec7799ee0bc90e31f3abfccff978eb2688a46
Kevin Chen <chenk.dev@gmail.com>
2018-01-28 15:38:31 -0600

    music files

---
+++ ggj2018/Assets/AudioSelecter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSelecter : MonoBehaviour {
+    public AudioClip[] songs;
+    AudioSource aud;
+	// Use this for initialization
+	void Start () {
+        aud = GetComponent<AudioSource>();
+
+        aud.clip = songs[Random.Range(0, songs.Length - 1)];
+        aud.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
